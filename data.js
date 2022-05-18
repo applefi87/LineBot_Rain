a26d082543e889743f0f4a78b909a45bfbb95557
applefi87 <werrtyyui@yahoo.com.tw>
2022-05-18 22:01:50 +0800

    確認好資料，拆解格式中

--- data.js
+++ data.js
@@ -1,82 +1,53 @@
 import axios from 'axios'
-import cheerio from 'cheerio'
-import template from './template.js'
-// import fs from 'fs'
+// import cheerio from 'cheerio'
+// import template from './template.js'
+import 'dotenv/config'
+import fs from 'fs'
+const key = process.env.WEATHER_KEY
 
 const getData = async function (e) {
   try {
-    const { data } = await axios.get('https://wdaweb.github.io/')
-    const $ = cheerio.load(data)
-    const card = $('#general .col-md-3:first-child')
-    const t = template
-    t.hero.url = 'https://wdaweb.github.io/' + card.find('img').attr('src').slice(2)
-    t.body.contents[0].text = card.find('.card-title a').text()
-    t.body.contents[1].text = card.find('.card-description').text()
+    // 用密鑰取出天氣預報綜合描述(涵蓋大多資料，用"。"分隔 )
+    // F-D0047-093 是可挑選鄉鎮2天資料+縣市名(最多五個)
+    // 代處理多縣市
+    const { data } = await axios.get(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-093?Authorization=${key}&locationId=F-D0047-001&elementName=WeatherDescription`)
+    // 抓取縣市名
+    const bigArea = data.records.locations[0].locationsName
+    // 將鄉鎮資料取陣列
+    const areasOrigin = data.records.locations[0].location
+    const areas = areasOrigin.map(ar => {
+      // 只取出關鍵的鄉鎮名、
+      const area = ar.locationName
+      const timeWeathersOrigin = ar.weatherElement[0].time
+      //  將時間再擷取
+      const timeWeathers = timeWeathersOrigin.map(t => {
+        const time = t.startTime
+        const valueOrigin = t.elementValue[0].value
+        const value = valueOrigin.split('。')
+        return { time, value }
+      })
+      return { area, timeWeathers }
+    })
+    fs.writeFileSync('test.json', JSON.stringify({ bigArea, areas }))
+    // const $ = cheerio.load(data)
+    // const card = $('#general .col-md-3:first-child')
+    // const t = template
+    // t.hero.url = 'https://wdaweb.github.io/' + card.find('img').attr('src').slice(2)
+    // t.body.contents[0].text = card.find('.card-title a').text()
+    // t.body.contents[1].text = card.find('.card-description').text()
 
-    const t2 = JSON.parse(JSON.stringify(t))
-    t2.body.contents[0].text = 't2'
-    e.reply(
-      [{
-        type: 'flex',
-        altText: '共通課程',
-        contents: {
-          type: 'carousel',
-          contents: [t, t, t, t, t, t, t, t, t, t, t, t]
-        }
-      }, {
-        type: 'flex',
-        altText: '共通課程',
-        contents: {
-          type: 'carousel',
-          contents: [t2, t2, t2, t2, t2, t2, t2, t2, t2, t2, t2, t2]
-        }
-      }, {
-        type: 'flex',
-        altText: '共通課程',
-        contents: {
-          type: 'carousel',
-          contents: [t, t, t, t, t, t, t, t, t, t, t, t]
-        }
-      }, {
-        type: 'template',
-        altText: 'this is a image carousel template',
-        template: {
-          type: 'image_carousel',
-          columns: [
-            {
-              imageUrl: 'https://picsum.photos/100/100',
-              title: 'title',
-              text: 'test',
-              action: {
-                type: 'postback',
-                label: 'Buy',
-                data: 'action=buy&itemid=111'
-              }
-            },
-            {
-              imageUrl: 'https://picsum.photos/100/200',
-              action: {
-                type: 'message',
-                label: 'Yes',
-                text: 'yes'
-              }
-            },
-            {
-              imageUrl: 'https://picsum.photos/100/100',
-              action: {
-                type: 'uri',
-                label: 'View detail',
-                uri: 'http://example.com/page/222'
-              }
-            }
-          ]
-        }
-      }, {
-        type: 'text',
-        text: '結尾'
-      }]
-
-    )
+    // const t2 = JSON.parse(JSON.stringify(t))
+    // t2.body.contents[0].text = 't2'
+    // e.reply(
+    //   [{
+    //     type: 'flex',
+    //     altText: '共通課程',
+    //     contents: {
+    //       type: 'carousel',
+    //       contents: [t, t, t, t, t, t, t, t, t, t, t, t]
+    //     }
+    //   }]
+    // )
   } catch (err) {
     console.log(err)
   }
