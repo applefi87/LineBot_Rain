babd0d179f09e8e81d0c6a1ed4e1d7de896906a7
applefi87 <werrtyyui@yahoo.com.tw>
2022-06-01 12:57:19 +0800

    test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const bot = linebot({
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
-const broadcastErr = function (place = 'here', err) {
+const broadcastErr = function (place = 'here:', err) {
   bot.broadcast({
     type: 'text',
     text: place + err
